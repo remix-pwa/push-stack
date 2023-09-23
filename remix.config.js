98417d8c7b91a9c66c38eb1335f64478e3185805
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-23 23:47:05 +0100

    Integrated Remix PWA into remix config

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,8 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import('@remix-pwa/dev').WorkerConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  tailwind: true,
+  serverDependenciesToBundle: [
+    /@remix-pwa\/.*/
+  ],
 };
