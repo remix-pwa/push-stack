de84e8ae4b058e843b5ac78e9c2d26f30e170296
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-24 16:22:27 +0100

    server files

---
+++ app/utils/server/env.server.ts
@@ -0,0 +1,23 @@
+/**
+ * This is used in both `entry.server.ts` and `root.tsx` to ensure that
+ * the environment variables are set and globally available before the app is
+ * started.
+ *
+ * NOTE: Do *not* add any environment variables in here that you do not wish to
+ * be included in the client.
+ * @returns all public ENV variables
+ */
+export function getEnv() {
+	return {
+		MODE: process.env.NODE_ENV,
+	}
+}
+
+type ENV = ReturnType<typeof getEnv>
+
+declare global {
+	var ENV: ENV
+	interface Window {
+		ENV: ENV
+	}
+}
