ad4a9fe2d12d0dcae66903c35d131e9d736ac00f
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-24 22:37:11 +0100

    upgrading sw

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,15 +1,20 @@
 /// <reference lib="WebWorker" />
 
-import { Storage } from '@remix-pwa/cache';
-import { cacheFirst, networkFirst } from '@remix-pwa/strategy';
-import type { DefaultFetchHandler } from '@remix-pwa/sw';
-import { RemixNavigationHandler, logger, matchRequest } from '@remix-pwa/sw';
+import { Storage } from "@remix-pwa/cache";
+import { cacheFirst, networkFirst } from "@remix-pwa/strategy";
+import type {
+  DefaultFetchHandler,
+  GetLoadContextFunction,
+} from "@remix-pwa/sw";
+import { RemixNavigationHandler, logger, matchRequest } from "@remix-pwa/sw";
+import db from "./utils/worker/storage";
+import type { GetLoadContextType } from "./utils/worker/types";
 
 declare let self: ServiceWorkerGlobalScope;
 
-const PAGES = 'page-cache';
-const DATA = 'data-cache';
-const ASSETS = 'assets-cache';
+const PAGES = "page-cache";
+const DATA = "data-cache";
+const ASSETS = "assets-cache";
 
 // Open the caches and wrap them in `RemixCache` instances.
 const dataCache = Storage.open(DATA, {
@@ -18,13 +23,13 @@ const dataCache = Storage.open(DATA, {
 const documentCache = Storage.open(PAGES);
 const assetCache = Storage.open(ASSETS);
 
-self.addEventListener('install', (event: ExtendableEvent) => {
-  logger.log('Service worker installed');
+self.addEventListener("install", (event: ExtendableEvent) => {
+  logger.log("Service worker installed");
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', (event: ExtendableEvent) => {
-  logger.log('Service worker activated');
+self.addEventListener("activate", (event: ExtendableEvent) => {
+  logger.log("Service worker activated");
   event.waitUntil(self.clients.claim());
 });
 
@@ -32,7 +37,7 @@ const dataHandler = networkFirst({
   cache: dataCache,
   cacheQueryOptions: {
     ignoreSearch: false,
-  }
+  },
 });
 
 const assetsHandler = cacheFirst({
@@ -43,20 +48,73 @@ const assetsHandler = cacheFirst({
   },
 });
 
+export const getLoadContext: GetLoadContextFunction = (event) => {
+  const { connectivity } = db();
+
+  return {
+    fetchFromServer: async () => {
+      const connectivityStatus = await connectivity
+        .get(1)
+        .then((data) => data?.status ?? "online");
+
+      if (connectivityStatus === "offline") {
+        return new Response(null, {
+          status: 503,
+          statusText: "Service Unavailable",
+        });
+      }
+
+      return fetch(event.request);
+    },
+    setConnectivityStatus: async (status: string) => {
+      await connectivity.put({ status, id: 1 });
+    },
+    getConnectivityStatus: async () => {
+      const data = await connectivity.get(1);
+      return data?.status ?? "online";
+    },
+    event,
+  };
+};
+
 // The default fetch event handler will be invoke if the
 // route is not matched by any of the worker action/loader.
-export const defaultFetchHandler: DefaultFetchHandler = ({ context, request }) => {
+export const defaultFetchHandler: DefaultFetchHandler = ({
+  context,
+  request,
+}) => {
+  const { fetchFromServer } = context as GetLoadContextType;
   const type = matchRequest(request);
-  
-  if (type === 'asset') {
+
+  if (request.destination == "image") {
+    console.log("image", request.url);
+    // if (request.method.toLowerCase() === "get") {
+    //   assetCache.put(request, new Response(null, { status: 202 }));
+    // } else {
+    //   assetCache
+    //     .match(request, {
+    //       ignoreSearch: true,
+    //       ignoreVary: true,
+    //     })
+    //     .then((response) => {
+    //       if (response) {
+    //         assetCache.delete(request);
+    //       }
+
+    //       assetCache.put(request, new Response(null, { status: 202 }));
+    //     });
+    // }
+  }
+
+  if (type === "asset") {
     return assetsHandler(context.event.request);
   }
 
-  if (type === 'loader') {
+  if (type === "loader") {
     return dataHandler(context.event.request);
   }
 
-  return context.fetchFromServer();
+  return fetchFromServer();
 };
 
 const handler = new RemixNavigationHandler({
@@ -64,6 +122,6 @@ const handler = new RemixNavigationHandler({
   documentCache,
 });
 
-self.addEventListener('message', event => {
+self.addEventListener("message", (event) => {
   event.waitUntil(handler.handle(event));
 });
