171d6f8c0a26212fe5bd63d8cd9a439ee130460c
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-25 23:50:01 +0100

    an auth status checker for main app

--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -1,8 +1,10 @@
 import type { WorkerActionArgs, WorkerLoaderArgs } from "@remix-pwa/sw";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { Form, Outlet, useLoaderData } from "@remix-run/react";
 import { useEffect } from "react";
 import { typedjson } from "remix-typedjson";
 import { Input } from "~/components/ui/input";
+import { requireUserId } from "~/utils/server/user.server";
 import type { GetLoadContextType } from "~/utils/worker/types";
 
 export const workerAction = async ({ context }: WorkerActionArgs) => {
@@ -13,8 +15,10 @@ export const workerAction = async ({ context }: WorkerActionArgs) => {
   return null;
 }
 
-export const loader = async () => {
-  return null;
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const user = await requireUserId(request);
+
+  return user;
 }
 
 export const workerLoader = async ({ context }: WorkerLoaderArgs) => {
@@ -30,14 +34,6 @@ export const workerLoader = async ({ context }: WorkerLoaderArgs) => {
 }
 
 export default function DashboardLayout() {
-  // const data = useLoaderData<typeof workerLoader>();
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     console.log(data);
-  //   }, 5_000);
-  // }, []);
-
   return (
     <div className="flex flex-col w-full h-screen">
       <header className="flex content-center justify-between w-full h-16 shadow-md">
