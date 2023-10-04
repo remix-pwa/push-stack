7c1e1d4b4c8b7c8a2425c8452389951cf1a57f98
Abdur-Rahman <abbadshaf05@gmail.com>
2023-10-04 14:05:34 +0100

    chore: new `Device` schema - simplified it

---
+++ prisma/migrations/20231004124431_simplified_push/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `mobile` on the `Device` table. All the data in the column will be lost.
+  - You are about to drop the column `platform` on the `Device` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Device" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "p256dh" TEXT NOT NULL,
+    "auth" TEXT NOT NULL,
+    "endpoint" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Device_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Device" ("auth", "createdAt", "endpoint", "id", "p256dh", "updatedAt", "userId") SELECT "auth", "createdAt", "endpoint", "id", "p256dh", "updatedAt", "userId" FROM "Device";
+DROP TABLE "Device";
+ALTER TABLE "new_Device" RENAME TO "Device";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
